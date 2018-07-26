4893f5d8ea81d286382a8149bc07bd4616864bb6
lijingze2008 <lijingze2008@qq.com>
2018-07-26 16:27:46 +0800

    base

---
+++ demo/VpnDemo/VpnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VpnDemo
+//
+//  Created by admin on 2018/7/25.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
